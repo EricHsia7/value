781e123da77339746a8b1a86b3ff228e9d635cd8
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-16 10:55:43 +0800

    update

--- src/interface/pages/index.ts
+++ src/interface/pages/index.ts
@@ -2,3 +2,17 @@ export type HomePage = 'home';
 export type SettingsPage = 'settings';
 export type EditorPage = 'editor';
 export type Page = HomePage | SettingsPage | EditorPage;
+
+export function getSwitchFunction(page: Page): Function {
+  switch (page) {
+    case 'home':
+      
+      break;
+    case 'settings':
+      break;
+    case 'editor':
+      break;
+    default:
+      break;
+  }
+}
--- src/interface/tabs/index.ts
+++ src/interface/tabs/index.ts
@@ -1,22 +1,126 @@
+import { documentQuerySelector, elementQuerySelector, elementQuerySelectorAll } from '../../lib/selector/index';
+import { booleanToString } from '../../lib/tools/boolean-to-string';
 import { generateIdentifier } from '../../lib/tools/generate-identifier';
+import { getIconHTML } from '../icons/index';
+import { MaterialSymbols } from '../icons/material-symbols-type';
 import { Page } from '../pages/index';
 
 export interface Tab {
   page: Page;
   name: string;
+  icon: MaterialSymbols;
   closable: boolean;
   parameters: Array<any>;
   id: string;
 }
 
+const tabsElement = documentQuerySelector('.css_tabs');
+
 const Tabs: { [TabID: Tab['id']]: Tab } = {};
 const TabsIndex: Array<Tab['id']> = [];
 
-export function registerTab(page: Tab['page'], name: Tab['name'], closable: Tab['closable'], parameters: Tab['parameters']): void {
+let previousTabs: Array<Tab> = [];
+
+function generateTabElement(): HTMLElement {
+  const newTabElement = document.createElement('div');
+  newTabElement.classList.add('css_tab');
+
+  const newIconElement = document.createElement('div');
+  newIconElement.classList.add('css_tab_icon');
+  newTabElement.appendChild(newIconElement);
+
+  const newNameElement = document.createElement('div');
+  newNameElement.classList.add('css_tab_name');
+  newTabElement.appendChild(newNameElement);
+
+  const newCloseElement = document.createElement('div');
+  newCloseElement.classList.add('css_tab_close');
+  newCloseElement.innerHTML = getIconHTML('close');
+  newTabElement.appendChild(newCloseElement);
+
+  return newTabElement;
+}
+
+function updateTabs(): void {
+  function updateTab(thisTabElement: HTMLElement, currentTab: Tab, previousTab: Tab | undefined): void {
+    function updateIcon(thisTabElement: HTMLElement, currentTab: Tab) {
+      const iconElement = elementQuerySelector(thisTabElement, '.css_tab_icon');
+      iconElement.innerHTML = getIconHTML(currentTab.icon);
+    }
+
+    function updateName(thisTabElement: HTMLElement, currentTab: Tab) {
+      const nameElement = elementQuerySelector(thisTabElement, '.css_tab_name');
+      nameElement.innerText = currentTab.name;
+    }
+
+    function updateClosable(thisTabElement: HTMLElement, currentTab: Tab) {
+      thisTabElement.setAttribute('closable', booleanToString(currentTab.closable));
+      if (currentTab.closable) {
+        // TODO: add closeTab
+      } else {
+        // TODO: remove closeTab
+      }
+    }
+
+    function updateOnclick(thisTabElement: HTMLElement, currentTab: Tab) {
+      thisTabElement.onclick = function () {
+        // TODO: switch pages
+      };
+    }
+
+    if (previousTab !== undefined) {
+      if (currentTab.icon !== previousTab.icon) {
+        updateIcon(thisTabElement, currentTab);
+      }
+      if (currentTab.name !== previousTab.name) {
+        updateName(thisTabElement, currentTab);
+      }
+      if (currentTab.closable !== previousTab.closable) {
+        updateClosable(thisTabElement, currentTab);
+      }
+      if (currentTab.page !== previousTab.page) {
+        updateOnclick(thisTabElement, currentTab);
+      }
+    } else {
+      updateIcon(thisTabElement, currentTab);
+      updateName(thisTabElement, currentTab);
+      updateClosable(thisTabElement, currentTab);
+      updateOnclick(thisTabElement, currentTab);
+    }
+  }
+  const tabElements = Array.from(elementQuerySelectorAll(tabsElement, '.css_tab'));
+  const tabElementsLength = tabElements.length;
+
+  const TabsLength = Tabs.length;
+  const fragment = new DocumentFragment();
+  if (TabsLength >= tabElementsLength) {
+    for (let i = tabElementsLength; i < TabsLength; i++) {
+      const newTabElement = generateTabElement();
+      fragment.appendChild(newTabElement);
+      tabElements.push(newTabElement);
+    }
+    tabsElement.append(fragment);
+  } else {
+    for (let j = tabElementsLength - 1; j >= TabsLength; j--) {
+      tabElements[j].remove();
+    }
+  }
+
+  for (let k = 0; k < TabsLength; k++) {
+    const previousTab = previousTabs[k];
+    const currentTab = Tabs[k];
+    const thisTabElement = tabElements[k];
+    updateTab(thisTabElement, currentTab, previousTab);
+    previousTabs.splice(k, 1, currentTab);
+  }
+}
+
+export function registerTab(page: Tab['page'], name: Tab['name'], icon: Tab['icon'], closable: Tab['closable'], parameters: Tab['parameters']): void {
   const TabID = generateIdentifier();
   const object: Tab = {
     page: page,
     name: name,
+    icon: icon,
     closable: closable,
     parameters: parameters,
     id: TabID
@@ -29,4 +133,5 @@ export function registerTab(page: Tab['page'], name: Tab['name'], closable: Tab[
 export function initializeTabs(): void {
   registerTab('home', 'Home', false, []);
   registerTab('settings', 'Settings', false, []);
+  updateTabs();
 }
