5f0efbc713a30b123c66702937cb22284781e2b8
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-18 00:42:34 +0800

    update

--- src/interface/pages/home/symbols/index.ts
+++ src/interface/pages/home/symbols/index.ts
@@ -1,4 +1,4 @@
-import { documentQuerySelector, elementQuerySelector } from '../../../../lib/selector/index';
+import { documentQuerySelector, elementQuerySelector, elementQuerySelectorAll } from '../../../../lib/selector/index';
 import { EvaluatedSymbol, listEvaluatedSymbols } from '../../../../lib/symbol/index';
 import { getIconHTML } from '../../../icons/index';
 
