a4fc2bbe0fbb8537e3353d004b81a13c393c72a0
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-17 16:07:05 +0800

    update

--- src/interface/pages/editor/index.ts
+++ src/interface/pages/editor/index.ts
@@ -1,11 +1,7 @@
-import { documentQuerySelector, elementQuerySelector } from '../../../lib/selector/index';
-import { getSymbol, Symbol } from '../../../lib/symbol/index';
+import { documentQuerySelector } from '../../../lib/selector/index';
+import { Symbol } from '../../../lib/symbol/index';
 
 const editorPage = documentQuerySelector('.css_editor_page');
-const editorHeadElement = elementQuerySelector(editorPage, '.css_editor_head');
-const editorHeadButtonLeftElement = elementQuerySelector(editorHeadElement, '.css_editor_head_button_left');
-
-let currentSymbolID: Symbol['id'] = '';
 
 export function showEditorPage(SymbolID: Symbol['id']): void {
   editorPage.setAttribute('displayed', 'true');
@@ -13,10 +9,4 @@ export function showEditorPage(SymbolID: Symbol['id']): void {
 
 export function hideEditorPage(): void {
   editorPage.setAttribute('displayed', 'false');
-}
-
-async function updateEditorPage(SymbolID: Symbol['id']) {
-  const thisSymbolObject = getSymbol(SymbolID);
-  if (thisSymbolObject !== undefined) {
-  }
 }
